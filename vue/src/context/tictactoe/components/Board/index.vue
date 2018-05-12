6f0e59a904181b0619ef3e2bfc1f96817151e1d5
Yoshiaki Itakura <itathirdchildnegi@gmail.com>
2018-05-12 21:26:15 +0900

    Tutorial improvement completed
    Improvement of the tutorial is completed.

--- vue/src/context/tictactoe/components/Board/index.vue
+++ vue/src/context/tictactoe/components/Board/index.vue
@@ -5,6 +5,7 @@
         <Square v-for="(v, j) in edgeLength"
           :key="'square-' + ((edgeLength * i) + j)"
           :value="squares[((edgeLength * i) + j)]"
+          :highlight="winningLine.includes((edgeLength * i) + j)"
           :onClick="onClick.bind(this, (edgeLength * i) + j)"
         />
       </div>
@@ -20,6 +21,10 @@ export default {
   name: "Board",
   props: {
     squares: Array,
+    winningLine: {
+      type: Array,
+      default: () => { return [] }
+    },
     onClick: {
       type: Function,
       required: true
--- vue/src/context/tictactoe/components/Game/index.vue
+++ vue/src/context/tictactoe/components/Game/index.vue
@@ -3,15 +3,16 @@
     <div class="game-board">
       <Board
         :squares="squares"
+        :winningLine="winner ? winner.line : []"
         :onClick="handleClick"
       />
     </div>
     <div class="game-info">
       <div class="status">{{ status }}</div>
-      <button>order</button>
+      <button @click="toggleOrder()">order {{ orderIsAsc ? '▲' : '▼' }}</button>
       <ol>
-        <li v-for="(v, i) in moves" :key="'move-' + v">
-          <button @click="jumpTo(i)" :class="{ emphasis: current === i }">{{ v }}</button>
+        <li v-for="v in moves" :key="'move-' + v.turn">
+          <button @click="jumpTo(v.turn)" :class="{ emphasis: current === v.turn }">{{ v.desc }}</button>
         </li>
       </ol>
     </div>
@@ -32,14 +33,17 @@ function calculateWinner(squares) {
     [2, 5, 8],
     [0, 4, 8],
     [2, 4, 6],
-  ];
+  ]
   for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
+    const [a, b, c] = lines[i]
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return {
+        name: squares[a],
+        line: [a, b, c]
+      }
     }
   }
-  return null;
+  return null
 }
 
 export default {
@@ -54,7 +58,9 @@ export default {
         }
       }],
       current: 0,
-      xIsNext: true
+      winner: null,
+      xIsNext: true,
+      orderIsAsc: true
     }
   },
   computed: {
@@ -68,24 +74,29 @@ export default {
       return this.xIsNext ? 'X' : 'O'
     },
     status: function() {
-      const winner = calculateWinner(this.squares)
-      return winner ? `Winner: ${winner}` : `Next player: ${this.player}`
+      if (this.winner) { return `Winner: ${this.winner.name}` }
+      return this.squares.includes(null) ? `Next player: ${this.player}` : 'The game is a draw.'
     },
     moves: function() {
-      return this.history.map((step, move) => {
-        return (move ? `Go to move #${move}` : 'Go to game start') + ` (col: ${step.tap.col}, row: ${step.tap.row})`;
+      const m = this.history.map((step, move) => {
+        const desc = (move ? `Go to move #${move}` : 'Go to game start') + ` (col: ${step.tap.col}, row: ${step.tap.row})`
+        return {
+          turn: move,
+          desc
+        }
       })
+      return this.orderIsAsc ? m : m.reverse()
     }
   },
   methods: {
     handleClick: function(i) {
-      const squares = this.squares.slice();
-      if (calculateWinner(this.squares) || this.squares[i]) {
+      const squares = this.squares.slice()
+      if (this.winner || this.squares[i]) {
         return;
       }
       squares[i] = this.player
       const tap = ((i) => {
-        const v = i === 0 ? [1, 1] : [(i % this.edgeLength) + 1, parseInt(i / this.edgeLength) + 1];
+        const v = i === 0 ? [1, 1] : [(i % this.edgeLength) + 1, parseInt(i / this.edgeLength) + 1]
         return { col: v[0], row: v[1] }
       })(i);
       this.history = this.history.slice(0, (this.current + 1)).concat([{
@@ -94,10 +105,15 @@ export default {
       }])
       this.current = this.history.length - 1      
       this.xIsNext = !this.xIsNext
+      this.winner = calculateWinner(this.squares)
     },
     jumpTo: function(i) {
       this.current = i
       this.xIsNext = (i % 2) === 0
+      this.winner = calculateWinner(this.squares)
+    },
+    toggleOrder: function() {
+      this.orderIsAsc = !this.orderIsAsc
     }
   },
   components: {
