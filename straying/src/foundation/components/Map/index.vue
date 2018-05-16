1a845b748455dbb2d6766872101f0a5410952d7b
Yoshiaki Itakura <itathirdchildnegi@gmail.com>
2018-05-16 18:46:14 +0900

    [WIP] update straying
    [WIP] update straying

--- straying/src/foundation/components/Map/index.vue
+++ straying/src/foundation/components/Map/index.vue
@@ -1,36 +1,24 @@
 <template>
-  <div id="map" class="map"></div>
+  <div class="container">
+    <MapMenu class="menu"></MapMenu>
+    <MapContent class="content"></MapContent>
+  </div>
 </template>
 
 <script>
+import MapMenu from '../MapMenu'
+import MapContent from '../MapContent'
+
 export default {
   name: 'Map',
+  components: {
+    MapMenu,
+    MapContent
+  },
   props: {
     maps: {
       type: Object
     }
-  },
-  data() {
-    return {
-      mapObj: null
-    }
-  },
-  mounted() {
-    if (this.mapObj) {
-      console.log('The map already exists.')
-      return
-    }
-    const myLatlng = new this.maps.LatLng(35.6927224, 139.6926458)
-    this.mapObj = new this.maps.Map(document.getElementById('map'), {
-      fullscreenControl: false,
-      streetViewControl: false,
-      zoom: 15,
-      minZoom: 10,
-      maxZoom: 20,
-      center: myLatlng,
-      gestureHandling: 'auto',
-      scrollwheel: false
-    })
   }
 }
 </script>
--- straying/src/foundation/components/MapMenu/index.vue
+++ straying/src/foundation/components/MapMenu/index.vue
@@ -13,7 +13,7 @@
 
 <script>
 export default {
-  name: 'ListMenu',
+  name: 'MapMenu',
   data() {
     return {
       items: [
