1ea3d4a663d3dbcba39ab45746ed1e4bf8d85de1
Yoshiaki Itakura <itathirdchildnegi@gmail.com>
2018-05-29 23:29:33 +0900

    [WIP] add files
    [WIP] add files

---
+++ vue_vuex_ts/src/index.ts
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import router from './router'
+import store from './store'
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#root',
+  router,
+  store,
+  render: h => h(App)
+})
