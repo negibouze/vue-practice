d86d2e3f5773350675c3696e0863d76fa4726f58
Yoshiaki Itakura <itathirdchildnegi@gmail.com>
2018-07-02 18:28:00 +0900

    [WIP] update proto
    [WIP] update proto

--- proto/src/App.vue
+++ proto/src/App.vue
@@ -9,3 +9,11 @@ export default Vue.extend({
   name: 'app'
 })
 </script>
+
+<!-- global -->
+<style lang="styl">
+</style>
+
+<!-- this component only -->
+<style lang="styl" scoped>
+</style>
---
+++ proto/src/api/client.ts
@@ -0,0 +1,17 @@
+import { default as axios, AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
+import { Method } from './method';
+
+export class Client {
+
+    private api: AxiosInstance;
+
+    constructor(conf: AxiosRequestConfig) {
+        this.api = axios.create(conf)
+    }
+
+    protected async fetch<T>(method: Method, conf: AxiosRequestConfig): Promise<T> {
+        const apiConf = Object.assign(conf, { method });
+        const res: AxiosResponse = await this.api.request(apiConf);
+        return <T>res.data;
+    }
+}
