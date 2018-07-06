ff882a968142c10a86a4c45338ea4ea574081d5e
Yoshiaki Itakura <itathirdchildnegi@gmail.com>
2018-07-06 18:43:06 +0900

    [WIP] update proto
    [WIP] update proto

--- proto/src/App.vue
+++ proto/src/App.vue
@@ -3,17 +3,20 @@
     <TButton @onclick="click">ボタン</TButton>
     <TButton @onclick="click" type="primary">ボタン</TButton>
     <TButton @onclick="click" type="error">ボタン</TButton>
+    <Hamburger direction="" />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import TButton from '@/components/atoms/button'
+import Hamburger from '@/components/atoms/hamburger'
 
 export default Vue.extend({
   name: 'app',
   components: {
-    TButton
+    TButton,
+    Hamburger
   },
   methods: {
     click(val: string) {
