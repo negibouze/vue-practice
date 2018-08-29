7c27775bf9065a2a0bd1c0787e44fbfd1e054bc3
Yoshiaki Itakura <itathirdchildnegi@gmail.com>
2018-08-29 23:55:40 +0900

    [WIP] update proto
    [WIP] update proto

---
+++ proto/src/constants/map-status.ts
@@ -0,0 +1,9 @@
+enum MapStatus {
+  INITIAL = 'initial',
+  CLICK_ACCEPTED = 'click accepted',
+  DRAWING_SHAPE = 'drawing shape',
+  DREW_SHAPE = 'drew shape',
+  DREW_MARKER = 'drew marker',
+}
+
+export default MapStatus;
